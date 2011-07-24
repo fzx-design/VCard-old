bb00c4056b8860761b5d09f3d84da4a4ac45ce58
Hasky <Hasky@HaskymatoMacBook-Pro.local>
2011-07-24 22:41:44 +0800

    core data controllers added

---
+++ PushBox/CoreDataTableViewController.h
@@ -0,0 +1,22 @@
+//
+//  CoreDataTableViewController.h
+//  PushBox
+//
+//  Created by Xie Hasky on 11-7-24.
+//  Copyright 2011年 同济大学. All rights reserved.
+//
+
+#import "CoreDataViewController.h"
+
+@interface CoreDataTableViewController : CoreDataViewController<NSFetchedResultsControllerDelegate, 
+UITableViewDelegate, UITableViewDataSource> {
+    NSFetchedResultsController *_fetchedResultsController;
+    UITableView *_tableView;
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath;
+
+@end
