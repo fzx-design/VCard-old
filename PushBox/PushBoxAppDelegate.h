13e6ecb6862cacda5ca1ad48667e3d5d763cd89a
Hasky <Hasky@HaskymatoMacBook-Pro.local>
2011-07-24 13:58:43 +0800

    Initial Commit

---
+++ PushBox/PushBoxAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  PushBoxAppDelegate.h
+//  PushBox
+//
+//  Created by Xie Hasky on 11-7-24.
+//  Copyright 2011年 同济大学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PushBoxAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ PushBox/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PushBox
+//
+//  Created by Xie Hasky on 11-7-24.
+//  Copyright 2011年 同济大学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
